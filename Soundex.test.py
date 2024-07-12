f72974a7065f5219da6c6252d1bae9eae79a9aab
Naveens08 <60317324+Naveens08@users.noreply.github.com>
2024-07-13 00:47:27 +0530

    Update Soundex.test.py

--- Soundex.test.py
+++ Soundex.test.py
@@ -9,6 +9,14 @@ class TestSoundex(unittest.TestCase):
     def test_single_character(self):
         self.assertEqual(generate_soundex("A"), "A000")
 
+    def test_two_characters(self):
+        self.assertEqual(generate_soundex("ZZ"), "Z000")
+
+    def test_three_characters(self):
+        self.assertEqual(generate_soundex("ZMN"), "Z500")
+
+    def test_two_characters(self):
+        self.assertEqual(generate_soundex("ZMNZ"), "Z520")
     
 if __name__ == '__main__':
     unittest.main()
